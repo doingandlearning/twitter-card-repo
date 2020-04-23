036cf9c5b645714a4eeeda936758638be8ac5010
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-04-23 08:44:34 +0100

    Updating cloudname

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -1,7 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 const qs = require("querystring");
 cloudinary.config({
-  cloud_name: "sector",
+  cloud_name: "kc-cloud",
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 });
@@ -10,15 +10,16 @@ exports.handler = async function (event, ctx) {
   const { queryStringParameters } = event;
 
   try {
+    // https://res.cloudinary.com/kc-cloud/image/upload/v1587626740/ogimages/img-1.png
     const imageUrl = cloudinary.url(
-      `${process.env.KEVIN_IMAGE_VERSION}/ogimages/img-1.png`,
+      `${process.env.KEVIN_IMAGE_VERSION}/og-images/img-1.png`,
       {
         // resouce_type: "raw"
         sign_url: true,
         // secure: true,
         custom_pre_function: {
           function_type: "remote",
-          source: `https://hungry-brattain-538c0b.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
+          source: `https://relaxed-payne-d1bfbe.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
             queryStringParameters
           )}`,
         },
