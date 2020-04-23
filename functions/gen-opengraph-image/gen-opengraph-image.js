948e84f798404d0b38387aef7734679e92034fde
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-04-23 08:48:35 +0100

    hardcode author

--- functions/gen-opengraph-image/gen-opengraph-image.js
+++ functions/gen-opengraph-image/gen-opengraph-image.js
@@ -30,7 +30,7 @@ exports.handler = async function (event, ctx) {
     content: `
   window.title = "${queryStringParameters.title || "No Title"}";
   window.tags = ${JSON.stringify(tags)};
-  window.author = "${queryStringParameters.author || ""}";
+  window.author = "@dolearning";
   `,
   });
   await page.addScriptTag({ content: script });
