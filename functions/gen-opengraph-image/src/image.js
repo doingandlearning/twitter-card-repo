54d26b3677404dd073e1ff78983645f3405208f8
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-04-25 17:38:03 +0100

    Uses scaletext.

--- functions/gen-opengraph-image/src/image.js
+++ functions/gen-opengraph-image/src/image.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx, Global } from "@emotion/core";
-import Textfit from "react-textfit";
+import ScaleText from "react-scale-text";
 import { render } from "react-dom";
 
 export default function Image() {
@@ -48,21 +48,14 @@ export default function Image() {
         <h1
           css={{
             color: "#EBECFA",
-            height: "100%",
             textAlign: "left",
+            marginLeft: "20px",
+            height: "80%",
           }}
         >
-          <Textfit
-            max={256}
-            min={24}
-            style={{
-              minHeight: "80%",
-              maxHeight: "80%",
-              lineHeight: 1,
-            }}
-          >
-            {window.title}
-          </Textfit>
+          <ScaleText minFontSize={24} maxFontSize={256}>
+            How to make use of API Routes in NextJS
+          </ScaleText>
         </h1>
         <div
           css={{
